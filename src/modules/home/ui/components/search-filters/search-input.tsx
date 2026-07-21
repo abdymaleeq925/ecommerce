37d4d81efa7108d2c887d60794aad264248eb1ee
Abdymalik Batyrkulov <abdymalikbatyrkulov@Abdymaliks-MacBook-Air.local>
2026-07-21 14:38:14 +0300

    erased useState in search-input.tsx

--- src/modules/home/ui/components/search-filters/search-input.tsx
+++ src/modules/home/ui/components/search-filters/search-input.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useState } from "react"
 import Link from "next/link"
 import { BookmarkCheckIcon, ListFilterIcon, SearchIcon } from "lucide-react"
 import { useQuery } from "@tanstack/react-query"
