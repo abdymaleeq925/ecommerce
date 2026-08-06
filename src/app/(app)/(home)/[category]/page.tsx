9b7e7a00269bfebde4200fe4c0386a26f3d63b58
Abdymalik Batyrkulov <abdymalikbatyrkulov@Abdymaliks-MacBook-Air.local>
2026-08-06 10:25:11 +0300

    changed prefetchQuery to prefetchInfiniteQuery in category Page

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -4,6 +4,7 @@ import type { SearchParams } from "nuqs/server";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { loadProductFilters } from "@/modules/products/search-params";
 import { ProductListView } from "@/modules/products/ui/views/product-list-views";
+import { DEFAULT_LIMIT } from "@/constants";
 interface CategoriesProps {
   params: Promise<{ category: string }>,
   searchParams: Promise<SearchParams>
@@ -14,7 +15,14 @@ const Page = async ({ params, searchParams }: CategoriesProps) => {
   const filters = await loadProductFilters(searchParams);
 
   const queryClient = getQueryClient();
-  await queryClient.prefetchQuery(trpc.products.getMany.queryOptions({ category, ...filters }));
+  await queryClient.prefetchInfiniteQuery(trpc.products.getMany.infiniteQueryOptions(
+    { category, limit: DEFAULT_LIMIT, ...filters },
+    {
+      getNextPageParam: (lastPage) => {
+        return lastPage.docs.length > 0 ? lastPage.nextPage : undefined;
+      }
+    }
+  ));
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <ProductListView category={category}/>
